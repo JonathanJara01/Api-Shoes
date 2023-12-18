03186e3d86a026e46c48ad1333c50a3a5f775c72
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-12-18 13:15:06 -0500

    commit # 8
    deploy back

--- Api-Shoes-Back/index.js
+++ Api-Shoes-Back/index.js
@@ -4,7 +4,7 @@ const server = require('./src/app.js');
 require('./src/dataBase.js');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3002;
+const {PORT} = process.env || 3002;
 
 server.listen(PORT, ()=>{
     console.log(`Escuchando en el puerto: ${PORT}`)
